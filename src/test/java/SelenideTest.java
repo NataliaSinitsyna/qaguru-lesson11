14248495babb674f8308e56194f5ee6c85e1ba02
natalya.sinitsyna <natalia.a.sinitsyna@gmail.com>
2023-07-28 08:47:16 +0200

    remove don't used imports

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,11 +1,7 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
